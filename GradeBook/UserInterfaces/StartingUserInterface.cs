a1715609f48bc143eb575d57d4848ed9621c2e70
kswizzlek <kid7kruger@gmail.com>
2018-07-24 14:59:43 -0400

    fixed bugs

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -39,15 +39,19 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[2];
+            var type = parts[2].ToLower();
+
+            BaseGradeBook gradeBook;
             if (type == "standard")
-            {
-                StandardGradeBook gradeBook = new StandardGradeBook(name);
-            }
+                gradeBook = new StandardGradeBook(name);
             else if(type == "ranked")
+                gradeBook = new RankedGradeBook(name);
+            else
             {
-                RankedGradeBook gradeBook = new RankedGradeBook(name);
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
+                return;
             }
+
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
         }
